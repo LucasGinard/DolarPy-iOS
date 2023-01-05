8e5462f6eb504d738e4fd51e9a904d2487c26f7e
Lucas Ginard <contactolucasginard@gmail.com>
2023-01-05 15:01:07 -0300

    init project

---
+++ DolarPy/DolarPy/DolarPyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DolarPyApp.swift
+//  DolarPy
+//
+//  Created by LucasG on 2023-01-03.
+//
+
+import SwiftUI
+
+@main
+struct DolarPyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DolarPy/DolarPyUITests/DolarPyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DolarPyUITestsLaunchTests.swift
+//  DolarPyUITests
+//
+//  Created by LucasG on 2023-01-03.
+//
+
+import XCTest
+
+final class DolarPyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
