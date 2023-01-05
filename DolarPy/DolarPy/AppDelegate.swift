3a78af946446bc1cc8e27ea8f9572055c291ae62
Lucas Ginard <contactolucasginard@gmail.com>
2023-01-05 15:28:18 -0300

    implement analytics firebase

---
+++ DolarPy/DolarPy/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  DolarPy
+//
+//  Created by LucasG on 2023-01-05.
+//
+
+import UIKit
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
--- DolarPy/DolarPy/DolarPyApp.swift
+++ DolarPy/DolarPy/DolarPyApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct DolarPyApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
