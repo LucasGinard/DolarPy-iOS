33fd2c1cdadf10d19195714952d1a2efd8672e1c
Lucas Ginard <contactolucasginard@gmail.com>
2023-03-06 17:49:43 -0300

    changes for ui dynamic with input

--- DolarPy/DolarPy/modules/home/view/HomeView.swift
+++ DolarPy/DolarPy/modules/home/view/HomeView.swift
@@ -11,34 +11,23 @@ struct HomeView: View {
     @State private var quotations = Array<QuotationModel>()
     @State var amountInput: String = ""
     @State var lastUpdate: String = ""
-    
+    @FocusState var isInputActive: Bool
+
     var body: some View {
         VStack(alignment: .center) {
             Text("💸 DolarPy 💸").padding()
             TextField("Monto", text: $amountInput)
-                .onChange(of: amountInput) { newValue in
-                    
-                }
+                .keyboardType(.numberPad)
                 .textFieldStyle(.roundedBorder).padding()
-            
-            let columns = [
-                GridItem(.fixed(160)),
-                GridItem(.flexible()),
-            ]
-            
-            ScrollView {
-                LazyVGrid(columns: columns, spacing: 20) {
-                    ForEach(quotations.indices, id: \.self) { item in
-                        let ref = self.calculateQuotation(amount: quotations[item].referencial_diario) ?? nil
-                        QuotationRowView(quotation: quotations[item], buy: self.calculateQuotation(amount: quotations[item].compra)!,sell: self.calculateQuotation(amount: quotations[item].venta)!,ref: ref)
+                .focused($isInputActive)
+                .toolbar {
+                    ToolbarItem(placement: .keyboard) {
+                        Button("Cerrar") {
+                            isInputActive = false
+                        }.padding([.trailing])
                     }
                 }
-                .padding(.horizontal)
-                .task {
-                    await self.getListQuotationService()
-                }
-                Text("Actualizado: \(lastUpdate)").padding()
-            }
+            self.QuotationsRowsView()
         }
         .padding()
         
@@ -48,7 +37,6 @@ struct HomeView: View {
         if amount == nil{
             return nil
         }
-        print(" se refresca venta -> \((Double(amountInput) ?? 1) * amount!)")
 
         return (Double(amountInput) ?? 1) * amount!
     }
@@ -66,6 +54,77 @@ struct HomeView: View {
             
         }catch {
             print("Error service")
+        }
+    }
+    
+    func QuotationsRowsView()-> some View{
+        let columns = [
+            GridItem(.fixed(160)),
+            GridItem(.flexible()),
+        ]
+        return ScrollView {
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(quotations.indices, id: \.self) { position in
+                    let ref = self.calculateQuotation(amount: quotations[position].referencial_diario) ?? nil
+                    let buy = self.calculateQuotation(amount: quotations[position].compra)!
+                    let sell = self.calculateQuotation(amount: quotations[position].venta)!
+                    
+                    VStack(alignment: .leading){
+                        VStack{
+                            if ref == nil{
+                                Spacer()
+                            }
+                            
+                            HStack{
+                                Text("Compra:")
+                                    .foregroundColor(.white)
+                                Spacer()
+                                Text(String(buy.formatDecimal()))
+                                    .foregroundColor(.white)
+                            }.padding(8)
+                            
+                            if ref == nil{
+                                Spacer()
+                            }
+                            
+                            HStack{
+                                Text("Venta:")
+                                    .foregroundColor(.white)
+                                Spacer()
+                                Text(String(sell.formatDecimal()))
+                                    .foregroundColor(.white)
+                            }.padding(8)
+                            
+                            if ref == nil{
+                                Spacer()
+                            }
+                            
+                            if let refDaily = ref {
+                                HStack{
+                                    Text("Ref Día:")
+                                        .foregroundColor(.white)
+                                    Spacer()
+                                    Text(String(refDaily)
+                                    )
+                                    .foregroundColor(.white)
+                                }.padding(8)
+                            }
+                        }
+                        VStack{
+                            if let name = quotations[position].name {
+                                Text(name).foregroundColor(Color(Colors.green_46B6AC))
+                                    .frame(maxWidth: .infinity,alignment: .leading).padding(8)
+                            }
+                        }.background(Color.white)
+                    }.frame(width: 160,height: 170)
+                        .background(Rectangle().fill(Color(Colors.green_46B6AC)).shadow(radius: 8))
+                }
+            }
+            .padding(.horizontal)
+            .task {
+                await self.getListQuotationService()
+            }
+            Text("Actualizado: \(lastUpdate)").padding()
         }
     }
 }
